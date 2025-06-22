ba3e11e64c12f7e6bfe47a1a25ec3a44e9400c20
Deepanshu Singh <anshu3012ep@gmail.com>
2025-06-23 00:42:46 +0530

    [ADD] debug symbol for editor + inventory plugin

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright DEEP-32
+
+
+#include "Player/Inv_PlayerController.h"
+
+#include "Inventory.h"
+
+void AInv_PlayerController::BeginPlay() {
+	Super::BeginPlay();
+	UE_LOG(LogInventory,Log,TEXT("Begin play for player controller"))
+}
