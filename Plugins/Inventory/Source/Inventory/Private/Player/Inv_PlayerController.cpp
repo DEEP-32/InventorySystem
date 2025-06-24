8003febc22c0fc39f198fbcc96149d25136fedb8
Deepanshu Singh <anshu3012ep@gmail.com>
2025-06-24 15:04:08 +0530

    [ADD] tracing logic for items

--- Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -7,9 +7,22 @@
 #include "EnhancedInputSubsystems.h"
 #include "Inventory.h"
 #include "Blueprint/UserWidget.h"
+#include "Engine/Engine.h"
+#include "Engine/GameViewportClient.h"
 #include "Engine/LocalPlayer.h"
+#include "Kismet/GameplayStatics.h"
 #include "Widgets/HUD/Inv_HUDWidget.h"
 
+
+AInv_PlayerController::AInv_PlayerController() {
+	PrimaryActorTick.bCanEverTick = true;
+	TraceLength = 500.0f;
+}
+
+void AInv_PlayerController::Tick(float DeltaSeconds) {
+	Super::Tick(DeltaSeconds);
+	TraceForItem();
+}
 
 void AInv_PlayerController::BeginPlay() {
 	Super::BeginPlay();
@@ -44,4 +57,39 @@ void AInv_PlayerController::CreateHUDWidget() {
 	if (IsValid(HUDWidget)) {
 		HUDWidget->AddToViewport();
 	}
+}
+
+void AInv_PlayerController::TraceForItem() {
+
+	if (!IsValid(GEngine) || !IsValid(GEngine->GameViewport)) return;
+	
+	FVector2D ViewportSize;
+	GEngine->GameViewport->GetViewportSize((ViewportSize));
+
+	const FVector2D ScreenCenter = ViewportSize / 2.0f;
+
+	FVector TraceStart;
+	FVector Forward;
+
+	if (!UGameplayStatics::DeprojectScreenToWorld(this,ScreenCenter,TraceStart,Forward))
+		return;
+
+	const FVector TraceEnd = TraceStart + (Forward * TraceLength);
+	FHitResult HitResult;
+
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ItemTraceChannel);
+
+	LastActor = ThisActor;
+	ThisActor = HitResult.GetActor();
+
+	if (ThisActor == LastActor) return;
+
+	if (ThisActor.IsValid()) {
+		UE_LOG(LogInventory, Warning, TEXT("Started tracing a new actor: %s"), *ThisActor->GetName());
+	}
+
+	if (LastActor.IsValid()) {
+		UE_LOG(LogInventory, Warning, TEXT("Stopped tracing last actor: %s"), *LastActor->GetName());
+	}
+	
 }
--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -17,6 +17,11 @@ UCLASS()
 class INVENTORY_API AInv_PlayerController : public APlayerController {
 	GENERATED_BODY()
 
+public:
+
+	AInv_PlayerController();
+	virtual void Tick(float DeltaSeconds) override;
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
@@ -24,6 +29,7 @@ protected:
 private:
 	void PrimaryInteract();
 	void CreateHUDWidget();
+	void TraceForItem();
 	
 
 	UPROPERTY(EditDefaultsOnly,Category = "Inventory|Input")
@@ -37,4 +43,14 @@ private:
 
 	UPROPERTY()
 	TObjectPtr<UInv_HUDWidget> HUDWidget;
+
+	UPROPERTY(EditDefaultsOnly,Category = "Inventory|Interaction")
+	double TraceLength;
+
+	UPROPERTY(EditDefaultsOnly,Category = "Inventory|Interaction")
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel;
+
+	TWeakObjectPtr<AActor> ThisActor = nullptr;
+	TWeakObjectPtr<AActor> LastActor = nullptr;
+	
 };
